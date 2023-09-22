f87c006ec972d9530a5f5792f6d98bd6a1932a73
chaerang <apotheosis408@naver.com>
2023-09-22 16:39:58 +0900

    calc_lect

---
+++ lect4_2.py
@@ -0,0 +1,28 @@
+# 콜백 함수
+# def prt_func(n):
+#     print("hello", n)
+    
+# def callfunc(fx):
+#     for i in range(5):
+#         fx(i)
+    
+# callfunc(prt_func)
+
+# def update_msg(name: str) -> list:
+#     msg = []
+#     msg.append("Hello," + name)
+#     msg.append("Bye," + name)
+#     return msg
+
+# def greeting(in_name: str) -> list:
+#     gt_msg = None
+#     gt_msg = update_msg(in_name)
+#     return gt_msg 
+
+# res = greeting("python")
+
+# for message in res:
+#     print(message)
+
+import calc
+
