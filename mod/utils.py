a36a9fcf80db6e7f84a74f29ce1c68b67d47cef4
chaerang <apotheosis408@naver.com>
2023-09-22 17:41:28 +0900

    calc_lect

---
+++ mod/utils.py
@@ -0,0 +1,19 @@
+import math
+import random
+
+x = random.randrage()
+
+def sq_res(x):
+    return math.sqrt(x)
+
+def sin_res():
+    return math.sin(math.pi /2)
+
+def e_res():
+    return math.log(math.e) 
+
+def exp_res(x):
+    return math.exp(x)
+
+def pi_res():
+    return math.pi
