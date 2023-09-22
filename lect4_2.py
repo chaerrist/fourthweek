5a9bdbdfb6f24337c6106399a3e6c6d271197d92
chaerang <apotheosis408@naver.com>
2023-09-22 17:29:50 +0900

    calc_lect

--- lect4_2.py
+++ lect4_2.py
@@ -24,5 +24,51 @@
 # for message in res:
 #     print(message)
 
-import calc
+# import calc
 
+# print(calc.add(8, 4))
+# print(calc.sub(8, 4))
+# print(calc.mul(8, 4))
+# print(calc.div(8, 4))
+
+
+import calc as cl
+
+print(cl.add(8, 4))
+print(cl.sub(8, 4))
+print(cl.mul(8, 4))
+print(cl.div(8, 4))
+
+
+
+import mod.circle_mod as cm
+
+print(cm.pi)
+t = int(input("숫자를 입력하시오."))
+print("%d 의 넓이는 %d 입니다.", t, cm.area(t))
+print("%d 의 둘레는 %d 입니다.", t, cm.circum(t))
+
+import mod.str_util as smod
+
+url = "https://www.notion.so/test/4-1/a1fe5ef0df1/41f7a1aa9ec01/3a859a"
+res = smod.cutstr(url, "/", 3)
+print(res)
+
+import math
+
+# 입력받은 수 제곱근 계산 : sqrt
+input_num = float(input("숫자를 입력하시오"))
+sq_res = math.sqrt(input_num)
+print(sq_res)
+# 삼각함수 계산 sin * pi / 2
+sin_res = math.sin(math.pi /2)
+print(sin_res)
+# 자연로그 x=y, x=e^y = log(e)
+e_res = math.log(math.e)
+print(e_res)
+# 지수계산 ln(x) = y, x= e^y, x=exp(y) 즉, exp(x)
+exp_res = math.exp(input_num)
+print(exp_res)
+# 파이계산 pi
+pi_res = math.pi
+print(pi_res)
---
+++ mod/circle_mod.py
@@ -0,0 +1,12 @@
+pi = 3.14159
+
+# 추가
+# 
+# 원주 구하기 : 반지름 * 2 * 파이
+# 원넓이 구하기 : 반지름 * 반지름 * 파이
+
+def circum(rad):
+    return rad * 2 * pi
+
+def area(rad):
+    return rad * rad * pi
